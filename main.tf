41789650770d1dcd08104adabd40499f9de4c846
anveshbhrdwj15 <anveshbhrdwj15@gmail.com>
2025-06-10 11:14:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "18_04-lts"
     version   = "latest"
   }
 
