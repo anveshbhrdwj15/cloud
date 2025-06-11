2e569d5fe7212c5e8e40e50bd99e7282bcdee20f
anveshbhrdwj15 <anveshbhrdwj15@gmail.com>
2025-06-11 15:45:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,23 +3,65 @@
 
 provider "azurerm" {
   features {}
-  subscription_id = "e7a256dc-769a-421c-9f8e-e283ed3cbefa"
+  subscription_id = "var.ARM_SUBSCRIPTION_ID"
+  default_tags {
+        tags = {
+          Project = "Udacity-deployment-project"
+            }
+      }
 }
 
+# Virtual network
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/22"]
+  address_space       = ["10.0.0.0/24"]
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 }
 
+#network security group
+resource "azurerm_network_security_group" "project_NSG" {
+  name                = "${var.prefix}-NSG"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+  
+  security_rule {
+       name                        = "allow-subnet-traffic"
+       priority                     = 100
+       direction                   = "Inbound"
+       access                      = "Allow"
+       protocol                    = "*" # Allow any protocol
+       source_port_range           = "*"  # Allow any port range
+       source_address_prefix       = "VirtualNetwork" # Allow from the subnet
+       destination_port_range      = "*"  # Allow any port range
+       destination_address_prefix = "VirtualNetwork" # Allow traffic to the subnet
+     
+   }
+   
+   security_rule {
+       name                        = "deny-access-from-internet"
+       priority                    = 120
+       direction                   = "Inbound"
+       access                      = "Deny"
+       protocol                    = "*" # Deny any protocol
+       source_port_range           = "*"  # Deny any port range
+       source_address_prefix       = "Internet" # from source
+       destination_port_range      = "*" 
+       destination_address_prefix = "VirtualNetwork"
+     
+   }
+}
+
+#subnet
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = "${var.resource_group}"
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+  network_security_group_id = azurerm_network_security_group.project_NSG.id
 }
 
+#network interface
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   resource_group_name = "${var.resource_group}"
@@ -30,6 +72,41 @@ resource "azurerm_network_interface" "main" {
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
+}
+
+resource "azurerm_lb" "load_balancer" {
+  name                = "${var.prefix}-lb"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+
+  frontend_ip_configuration {
+    name                 = "primary"
+    public_ip_address_id = azurerm_public_ip.load_balancer.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bknd_pool" {
+  loadbalancer_id = azurerm_lb.load_balancer.id
+  name            = "${var.prefix}-bknd-address-pool-lb"
+}
+
+
+resource "azurerm_network_interface_backend_address_pool_association" "addresspool_assn" {
+  network_interface_id    = azurerm_network_interface.main.id
+  ip_configuration_name   = "${var.prefix}-addresspool-assn"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bknd_pool.id
+}
+
+resource "azurerm_availability_set" "availability_set" {
+  name                = "${var.prefix}-aset"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
+
+}
+
+data "azurerm_image" "packer-image" {
+  name                = var.packer_image_name
+  resource_group_name = "${var.resource_group}"
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
